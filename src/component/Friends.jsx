e174bf9f591baca87a0829d28ddde323452ff954
robiat <robiatyetunde18@gmail.com>
2025-05-21 21:12:55 +0100

    my react practice

--- src/component/Friends.jsx
+++ src/component/Friends.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+
+const friends = [
+  { id: 1, name: "Alex", age: 24 },
+  { id: 2, name: "Maya", age: 22 },
+  { id: 3, name: "Jordan", age: 25 },
+  { id: 4, name: "Sam", age: 23 },
+  { id: 5, name: "Liam", age: 21 },
+];
 
 const Friends = () => {
   return (
     <div>
-      
+      {friends.map((data) => {
+        return <FriendsData key={data.id} {...data} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Friends
+const FriendsData = ({name, age}) => {
+  return (
+    <div>
+      <p>
+        {name} {age}
+      </p>
+    </div>
+  );
+};
+export default Friends;
